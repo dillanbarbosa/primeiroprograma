c7361b8678e86c8ac20e5de9516a03fffcd3f4eb
dillanbarbosa <dillan.invencao@escola.pr.gov.br>
2025-02-20 12:12:12 -0300

    Add files via upload

---
+++ primeiro.c
@@ -0,0 +1,16 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int denny;
+    denny=69;
+    int carlinhos;
+    carlinhos=24;
+    int soma=denny+carlinhos;
+    int multiplicacao;
+    int multiplicacao=denny*carlinhos;
+    printf("o valor de denny é %d \n",denny);
+    printf("o valor de carlinhos é %d \n",carlinhos);
+    printf("a soma é %d \n",soma);
+    printf("a multiplicacao é %d \n",multiplicacao);
+    return 0;
